7e8a78b3ec1e0e36b54cd664427026c00e489d5b
Tyler Mears <59263760+futureof723@users.noreply.github.com>
2025-01-24 05:27:46 -0500

    Update config.py
    Removed my owner id and token from config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-OWNER_ID = 656958839685382154
-TOKEN = 'MTMzMTI1NjEwNzM0MzE1NTI4Mw.GyCtXv.vIpdZsFcI3IY5XkB-JLTogGHgrnq9NrQxIsVs4'
+OWNER_ID = Enter your owner id here
+TOKEN = 'Enter your token here'
